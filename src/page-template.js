1876407863b7b6dd39fac5b383a49beb5a4c0263
Boba Fett <thomascupchurch@gmail.com>
2021-08-14 10:04:51 -0500

    add html structure template

--- src/page-template.js
+++ src/page-template.js
@@ -16,20 +16,13 @@ const generatePage = (name, github) => {
       <header>
         <div class="container flex-row justify-space-between align-center py-3">
           <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
-          <nav class="flex-row">
-            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-              header.github
-            }">GitHub</a>
-          </nav>
         </div>
       </header>
       <main class="container my-5">
         ${generateAbout(about)}
         ${generateProjects(projects)}
       </main>
-      <footer class="container text-center py-3">
-        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
-      </footer>
+      
     </body>
     </html>
     `;
