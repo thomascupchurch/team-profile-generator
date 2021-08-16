592bc3530a81bf2f8b7a61a9a3028b99531fbfb8
Boba Fett <thomascupchurch@gmail.com>
2021-08-15 19:02:58 -0500

    function fixes

--- index.js
+++ index.js
@@ -5,75 +5,95 @@ const generateTeam = require('./utils/generate-site');
 const { profile } = require('console');
 const { number } = require('yargs');
 
-function generateProfile() {
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-    console.log(`
-    =================
-    Create a new Team
-    =================
-    `);
+const teamArray = [];
 
-    return inquirer
+
+const managerQuestions = () => {
+    return (
+        inquirer
         .prompt([
             {
                 type: 'input',
                 name: 'managerName',
                 message: 'What is the name of the manager of this team?',
-                validate: managerNameInput => {
-                    if (managerNameInput) {
-                      return true;
-                    } else {
-                      console.log('You need to enter a manager name!');
-                      return false;
-                    }
-                }
+                // validate: managerNameInput => {
+                //     if (managerNameInput) {
+                //       return true;
+                //     } else {
+                //       console.log('You need to enter a manager name!');
+                //       return false;
+                //     }
+                // }
             },
             {
                 type: 'input',
                 name: 'managerId',
                 message: 'What is the ID for the manager?',
-                validate: managerIdInput => {
-                    const parsedId = parseInt(managerIdInput);
-                    if (managerIdInput && parsedId == managerIdInput) {
-                      return true;
-                    } else {
-                      console.log('You need to enter a manager ID!');
-                      return false;
-                    }
-                }
+                // validate: managerIdInput => {
+                //     const parsedId = parseInt(managerIdInput);
+                //     if (managerIdInput && parsedId == managerIdInput) {
+                //       return true;
+                //     } else {
+                //       console.log('You need to enter a manager ID!');
+                //       return false;
+                //     }
+                // }
             },
             {
                 type: 'input',
                 name: 'managerEmail',
                 message: 'What is the email address for this manager?',
-                validate: managerEmailInput => {
+                // validate: managerEmailInput => {
                     
-                    if (managerEmailInput.includes('@')) {
-                      return true;
-                    } else {
-                      console.log('You need to enter a valid email address.');
-                      return false;
-                    }
-                }
+                //     if (managerEmailInput.includes('@')) {
+                //       return true;
+                //     } else {
+                //       console.log('You need to enter a valid email address.');
+                //       return false;
+                //     }
+                // }
             },
             {
                 type: 'input',
                 name: 'managerPhone',
                 message: 'What is the ten digit office phone number for the manager?',
-                validate: managerPhoneInput => {
+                // validate: managerPhoneInput => {
                     
-                    if (managerPhoneInput.length === 10) {
-                      return true;
-                    } else {
-                      console.log('You need to enter a ten-digit (no other symbols or characters) office phone number.');
-                      return false;
-                    }
-                }
-            },
+                //     if (managerPhoneInput.length === 10) {
+                //       return true;
+                //     } else {
+                //       console.log('You need to enter a ten-digit (no other symbols or characters) office phone number.');
+                //       return false;
+                //     }
+                // }
+            }
+        ])
+        .then((answers) => {
+            const newManager = new Manager(
+                answers.managerNameInput,
+                answers.managerIdInput,
+                answers.managerEmailInput,
+                answers.managerOfficeNumberInput
+            )
+
+            teamArray.push(newManager);
+            console.log('Manager Added!')
+        }));
+        
+        
+    };
+    
+    const engineerQuestions = () => {
+    return inquirer
+        .prompt([
             {
                 type: 'input',
                 name: 'engineerName',
-                message: 'What is the name of the first engineer?',
+                message: 'What is the name of this engineer?',
                 validate: engineerNameInput => {
                     
                     if (engineerNameInput) {
@@ -93,7 +113,7 @@ function generateProfile() {
                     if (engineerIdInput && parsedId == engineerIdInput) {
                       return true;
                     } else {
-                      console.log('You need to enter a manager ID!');
+                      console.log('You need to enter an engineer ID!');
                       return false;
                     }
                 }
@@ -126,15 +146,129 @@ function generateProfile() {
                     }
                 }
             }
-            
-        
         ])
-}
+        .then((engAnswers) => {
+            const newEngineer = new Engineer(
+                engAnswers.engineerNameInput,
+                engAnswers.engineerIdInput,
+                engAnswers.engineerEmailInput,
+                engAnswers.engineerGithubInput
+            )
+            teamArray.push(newEngineer);
+            console.log('Engineer Added!');
+        })
+        
+    };
+
+
+    const internQuestions = () => {
+        return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: 'What is the name of this intern?',
+                validate: engineerNameInput => {
+                    
+                    if (engineerNameInput) {
+                      return true;
+                    } else {
+                      console.log('You need to enter the name of an intern.');
+                      return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'internId',
+                message: 'What is the Id for this intern?',
+                validate: internIdInput => {
+                    const parsedId = parseInt(internIdInput);
+                    if (internIdInput && parsedId == internIdInput) {
+                      return true;
+                    } else {
+                      console.log('You need to enter an intern ID!');
+                      return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'internEmail',
+                message: 'What is the email address for this intern?',
+                validate: internEmailInput => {
+                    
+                    if (internEmailInput.includes('@')) {
+                      return true;
+                    } else {
+                      console.log('You need to enter a valid email address.');
+                      return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'internSchool',
+                message: 'What school is this intern from?',
+                validate: internSchoolInput => {
+                    
+                    if (internSchoolInput) {
+                      return true;
+                    } else {
+                      console.log('You need to enter a github username.');
+                      return false;
+                    }
+                }
+            }
+        ])
+        .then((intAnswers) => {
+            const newIntern = new Intern(
+                intAnswers.internNameInput,
+                intAnswers.internIdInput,
+                intAnswers.internEmailInput,
+                intAnswers.internGithubInput
+            )
+            teamArray.push(newIntern);
+            console.logg('Intern Added!');
+        })
+        
+    }
+  
+    
 
 
 
-generateProfile();
+    console.log(`
+    =================
+    Create a new Team
+    =================
+    `)
+    
+    managerQuestions();
+    engineerQuestions();
+    internQuestions()
+
+
+        // .then(function(data) {
+        //     console.log(data);
+        //     const TeamArray = teamArray.push(data);
+        //     console.log(teamArray);
+
+        // })
+
+        .then(function() {
+            const generatePage = require('./src/page-template');
+            const generatedHtml = require('./src/page-template');
+        })
+        .then(function(teamArray) {
+            console.log(teamArray);
+            generateTeam.writeFile /*(JSON.stringify*/(generatedHtml)/*)*/;
+        });
+        
 
 
 
-// new profile().generateProfile();
+
+
+
+
--- src/page-template.js
+++ src/page-template.js
@@ -1,31 +1,85 @@
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="style.css">
-    </head>
+
+
+const inquirer = require("inquirer");
+const index = require('../index');
+const teamArray = index.teamArray;
+console.log(teamArray);
+// const myArr = arrayInfo.teamArray;
+let generatedHtml = '';
+
+  const generateTeam = (teamArray) => {
     
-    <body>
-      <header>
-        <div class="container flex-row justify-space-between align-center py-3">
-          <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+    teamArray.forEach((element) => {
+      if (element instanceof Manager) {
+        generatedHtml += `
+        <div class='card' style='width: 18rem;'>
+          <div class ='card-header'>
+          ${element.name}
+          </div>
+        <ul class='list-group list-group-flush'>
+          <li class='list-group-item'>${element.getRole()}</li>
+          <li class='list-group-item'>${element.id}</li>
+          <li class='list-group-item'>${element.getOfficeNumber()}</li>
+        </ul>
+        </div>`; 
+      } else if (element instanceof Engineer) {
+        generatedHtml += `
+        <div class='card' style='width: 18rem;'>
+        <div class='card-header'>
+        ${element.name}
         </div>
-      </header>
-      <main class="container my-5">
-        ${generateAbout(about)}
-        ${generateProjects(projects)}
-      </main>
-      
-    </body>
-    </html>
-    `;
+        <ul class='list-group list-group-flush'>
+          <li class='list-group-item'>${element.getRole()}</li>
+          <li class='list-group-item'>${element.id}</li>
+          <li class='list-group-item'>${element.getGithub()}</li>
+          </ul>
+        </div>`;
+      } else if (element instanceof Intern) {
+        generatedHtml += `
+        <div class='card' style='width: 18rem;'>
+        <div class='card-header'>
+        ${element.name}
+        </div>
+        <ul class='list-group list-group-flush'>
+          <li class='list-group-item'>${element.getRole()}</li>
+          <li class='list-group-item'>${element.id}</li>
+          <li class='list-group-item'>${element.getSchool()}</li>
+          </ul>
+        </div>`;
+      }
+    })
+
   };
 
-  module.exports = generatePage;
+  const htmlTemplate = 
+`
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3"></h1>
+      </div>
+    </header>
+    <main class="container my-5">
+    ${generateTeam()}
+    </main>
+    </body>
+    </html>
+    `
+    ;
+  generatedHtml += htmlTemplate;
+  
+  // generatedHtml += generateTeam;
+  
+  module.exports = generatedHtml;
